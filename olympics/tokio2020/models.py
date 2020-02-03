78427bf84a7c4f7c05a05c083586cc9902a7b98b
William Valero <William.Valero@wnco.com>
2020-02-03 15:29:59 -0500

    [WV] Models changes

--- olympics/tokio2020/models.py
+++ olympics/tokio2020/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 # Create your models here.
 from django.forms import PasswordInput
+from django.utils import timezone
 
 
 class Student(models.Model):
@@ -17,62 +18,42 @@ class Student(models.Model):
     def __str__(self):
         return self.firstName + ' ' + self.lastName
 
-    class Meta:
-        managed = False
-        db_table = u'"Student\'sTables\".\"students"'
-
 
 class Sport(models.Model):
-    name = models.CharField(max_length=60, db_column='name')
-    photo = models.CharField(max_length=300, db_column='photo')
+    name = models.CharField(max_length=60, db_column='name', null=True)
+    photo = models.CharField(max_length=300, db_column='photo', null=True)
 
     def __str__(self):
         return self.name
-
-    class Meta:
-        managed = False
-        db_table = u'"Athlete\'sTables\".\"sports"'
 
 
 class SportModality(models.Model):
-    sport = models.ForeignKey(Sport, on_delete=models.CASCADE, db_column='id_sport')
-    name = models.CharField(max_length=60, db_column='name')
+    sport = models.ForeignKey(Sport, on_delete=models.CASCADE, db_column='id_sport', null=True)
+    name = models.CharField(max_length=60, db_column='name', null=True)
 
     def __str__(self):
         return self.name
 
-    class Meta:
-        managed = False
-        db_table = u'"Athlete\'sTables\".\"sport_modalities"'
-
 
 class Event(models.Model):
-    sport = models.ForeignKey(Sport, models.CASCADE, db_column='id_sport')
-    sportModality = models.ForeignKey(SportModality, models.CASCADE, db_column='id_sport_modality')
+    sport = models.ForeignKey(Sport, models.CASCADE, db_column='id_sport', null=True)
+    sportModality = models.ForeignKey(SportModality, models.CASCADE, db_column='id_sport_modality', null=True)
     videoLink = models.CharField(max_length=100, db_column='video_link')
-    eventDate = models.DateField(db_column='event_date')
-    eventTime = models.TimeField(db_column='event_time')
+    eventDate = models.DateField(db_column='event_date', default=timezone.now)
+    eventTime = models.TimeField(db_column='event_time', default=timezone.now)
 
     def __str__(self):
         return self.sport.__str__() + ' ' + self.sportModality.__str__() + ' ' + self.eventDate.__str__() + ' ' + self.eventTime.__str__()
 
-    class Meta:
-        managed = False
-        db_table = u'"Event\'sTables\".\"events"'
-
 
 class EventVideoComments(models.Model):
-    event = models.ForeignKey(Event, on_delete=models.CASCADE, db_column='id_event')
-    student = models.ForeignKey(Student, on_delete=models.CASCADE, db_column='id_student')
-    creationDate = models.DateField(db_column='creation_date')
-    comment = models.CharField(max_length=500, db_column='comment')
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, db_column='id_event', null=True)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, db_column='id_student', null=True)
+    creationDate = models.DateField(db_column='creation_date', default=timezone.now)
+    comment = models.CharField(max_length=500, db_column='comment', null=True)
 
     def __str__(self):
         return self.event.__str__() + ' ' + self.student.__str__() + ' ' + self.creationDate.__str__()
-
-    class Meta:
-        managed = False
-        db_table = u'"Student\'sTables\".\"event_video_comments"'
 
 
 class Coach(models.Model):
@@ -82,13 +63,9 @@ class Coach(models.Model):
     def __str__(self):
         return self.firstName + ' ' + self.lastName
 
-    class Meta:
-        managed = False
-        db_table = u'"Athlete\'sTables\".\"coaches"'
-
 
 class Athlete(models.Model):
-    coach = models.ForeignKey(Coach, on_delete=models.CASCADE, db_column='id_coach')
+    coach = models.ForeignKey(Coach, on_delete=models.CASCADE, db_column='id_coach', null=True)
     names = models.CharField(max_length=100, db_column='names')
     lastName = models.CharField(max_length=100, db_column='last_name')
     birthDate = models.DateField(db_column='birth_date')
@@ -101,31 +78,19 @@ class Athlete(models.Model):
     def __str__(self):
         return self.names + ' ' + self.lastName
 
-    class Meta:
-        managed = False
-        db_table = u'"Athlete\'sTables\".\"athletes"'
-
 
 class AthleteSport(models.Model):
-    athlete = models.ForeignKey(Athlete, on_delete=models.CASCADE, db_column='id_athlete')
-    sport = models.ForeignKey(Sport, on_delete=models.CASCADE, db_column='id_sport')
-
-    class Meta:
-        managed = False
-        db_table = u'"Athlete\'sTables\".\"athletes_sports"'
+    athlete = models.ForeignKey(Athlete, on_delete=models.CASCADE, db_column='id_athlete', null=True)
+    sport = models.ForeignKey(Sport, on_delete=models.CASCADE, db_column='id_sport', null=True)
 
 
 class AthleteEvent(models.Model):
-    event = models.ForeignKey(Event, on_delete=models.CASCADE, db_column='id_event')
-    athlete = models.ForeignKey(Athlete, on_delete=models.CASCADE, db_column='id_athlete')
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, db_column='id_event', null=True)
+    athlete = models.ForeignKey(Athlete, on_delete=models.CASCADE, db_column='id_athlete', null=True)
     result = models.CharField(max_length=20, db_column='result')
 
     def __str__(self):
         return self.event.__str__() + ' ' + self.athlete.__str__()
-
-    class Meta:
-        managed = False
-        db_table = u'"Event\'sTables\".\"athlete_event"'
 
 
 class AthleteEventInline(admin.TabularInline):
@@ -151,10 +116,9 @@ class SportAdmin(admin.ModelAdmin):
 
 
 class StudentForm(forms.ModelForm):
-    class Meta:
-        widgets = {
-            'password': PasswordInput,
-        }
+    widgets = {
+        'password': PasswordInput,
+    }
 
 
 class StudentAdmin(admin.ModelAdmin):
